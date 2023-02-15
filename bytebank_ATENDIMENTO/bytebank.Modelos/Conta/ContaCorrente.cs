11252cfbf36f36202bbb126b942c2bd613bb7439
orafaelcarvalho <rsouzadecarvalho@gmail.com>
2023-02-14 22:02:30 -0300

    Final aula 5 e final curso de Arrays e Coleções

--- bytebank_ATENDIMENTO/bytebank.Modelos/Conta/ContaCorrente.cs
+++ bytebank_ATENDIMENTO/bytebank.Modelos/Conta/ContaCorrente.cs
@@ -129,6 +129,14 @@
             Conta = conta;
             Titular = new Cliente();
             TotalDeContasCriadas += 1;
+        }
+
+        public ContaCorrente(int numero_agencia)
+        {
+            Numero_agencia = numero_agencia;
+            Conta = Guid.NewGuid().ToString().Substring(0, 8);
+            Titular = new Cliente();
+            TotalDeContasCriadas += 1;
 
         }
 
@@ -150,12 +158,14 @@
         public override string ToString()
         {
 
-            return $" === DADOS DA CONTA === \n" +
+            return $"\n === DADOS DA CONTA === \n" +
                    $"Número da Conta : {this.Conta} \n" +
+                   $"Número da Agência : {this.Numero_agencia} \n" +
                    $"Saldo da Conta: {this.Saldo} \n" +
                    $"Titular da Conta: {this.Titular.Nome} \n" +
                    $"CPF do Titular  : {this.Titular.Cpf} \n" +
-                   $"Profissão do Titular: {this.Titular.Profissao}";
+                   $"Profissão do Titular: {this.Titular.Profissao} \n\n" +
+                   $"------------------------------------------------ \n";
         }
     }
 }
